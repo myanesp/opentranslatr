41879a03f422b3916670b048fa0a5c50fdb9d66c
myanesp <17320863+myanesp@users.noreply.github.com>
2024-02-28 12:35:03 +0000

    add: translation functions

---
+++ R/translate.R
@@ -0,0 +1,178 @@
+#' Translate strings using SimplyTranslate
+#'
+#' SimplyTranslate has support for three translation engines: Google Translate,
+#' inCIBA and Reverso. You can specify which one do you want to use session-wide
+#' or every time you execute the function. This function also helps with the selection
+#' of the SimplyTranslate instance you want to use.
+#'
+#' @param from Source language. Specify it with the code following the ISO 639 standard
+#' @param to Specify it with the code following the ISO 639 standard
+#' @param str Text to translate
+#' @param instance The SimplyTranslate URL you want to use
+#' @param engine The translation engine you want to use: "google", "reverso" or "inciba"
+#' @export
+#' @examples
+#' \dontrun{
+#' simplytranslate(from = "en", to = "es", str = "hello", instance = "https://simplytranslate.org)")
+#' }
+
+simplytranslate <- function(from, to, str, instance = NULL, engine = NULL) {
+  str <- utils::URLencode(str)
+
+  if (is.null(instance)) {
+    if (!nzchar(Sys.getenv("st_inst"))) {
+      message("You did not specify any SimplyTranslate instance.")
+      cat("\n")
+      message("Would you like to select the official one or to specify a custom one?")
+
+      instance <- utils::menu(
+        choices = c(
+          "Use the official one, simplytranslate.org",
+          "I'd prefer specifying a custom one"
+        )
+      )
+
+      if (instance == 1) {
+
+        Sys.setenv(st_inst = "https://simplytranslate.org")
+        message("Using the default one, simplytranslate.org")
+
+      } else {
+        st_instance <- readline("Please, write the URL of your instance, including http or https: ")
+
+        if (!grepl("^https?://", st_instance)) {
+
+          message("You have not provided a valid URL. Check it and be sure that contains http:// or https://.")
+          st_instance <- readline("Please, type it again: ")
+
+          if (!grepl("^https?://", st_instance)) {
+            stop("You have not provided a valid URL. Check it and be sure that contains http:// or https://.")
+          }
+        }
+
+        Sys.setenv(st_inst = st_instance)
+      }
+    }
+  }
+
+  if(is.null(engine)) {
+    if (!nzchar(Sys.getenv("st_eng"))) {
+      message("You did not select any of the available translation engines.")
+      cat("\n")
+      message("Please, select one. Keep in mind that the selected one will be the default engine for this session unless you declare one on the function.")
+
+      engine <- utils::menu(
+        choices = c(
+          "Google Translate",
+          "iCIBA",
+          "Reverso"
+        )
+      )
+
+      if (engine == 1){
+        Sys.setenv(st_eng = "google")
+        message("Using Google Translate engine")
+      } else if (engine == 2) {
+        Sys.setenv(st_eng = "iciba")
+        message("Using iCIBA engine")
+      } else if (engine == 3) {
+        Sys.setenv(st_eng = "reverso")
+        message("Using Reverso engine")
+      }
+    }
+    req <- httr2::request(Sys.getenv("st_inst")) |>
+      httr2::req_url_path_append("/api/translate") |>
+      httr2::req_url_query(engine = Sys.getenv("st_eng"), from = from, to = to, text = str) |>
+      httr2::req_perform()
+
+    resp <- req |>
+      httr2::resp_body_json()
+
+    translation <- resp$translated_text
+
+    return(translation)
+
+  } else if (!is.null(engine)) {
+      req <- httr2::request(Sys.getenv("st_inst")) |>
+        httr2::req_url_path_append("/api/translate") |>
+        httr2::req_url_query(engine = engine, from = from, to = to, text = str) |>
+        httr2::req_perform()
+
+      resp <- req |>
+        httr2::resp_body_json()
+
+      translation <- resp$translated_text
+
+      return(translation)
+  }
+}
+
+#' Translate strings using Lingva frontend
+#'
+#' Lingva is a privacy-respecting frontend for the Google Translate engine.
+#'
+#' @param from Source language. Specify it with the code following the ISO 639 standard
+#' @param to Specify it with the code following the ISO 639 standard
+#' @param str Text to translate
+#' @param instance The Lingva instance URL you want to use. If it's not set, it will ask you to select one
+#' @export
+#' @examples
+#' \dontrun{
+#' lingva(from = "en", to = "es", str = "hello")
+#' }
+
+lingva <- function(from, to, str, instance = NULL) {
+  if(is.null(instance)){
+
+    if (!nzchar(Sys.getenv("ling_inst"))) {
+      message("You have not choose an instance.")
+      cat("\n")
+      message("Would you like to select the official one or to specify a custom one?")
+      cat("\n")
+      message("Please, keep in mind that the mantainer of the official one has asked to not abuse the API as the costs of the bandwidth are expensive.")
+
+      instance <- utils::menu(
+        choices = c(
+          "I'm going to use responsibly the official one, lingva.thedaviddelta.com",
+          "I'd prefer specifying a custom one"
+        )
+      )
+
+      if (instance == 1){
+        message("Selected the official one.")
+        Sys.setenv(ling_inst = "https://lingva.thedaviddelta.com")
+      } else {
+        ling_instance <- readline("Please, write the URL of your instance, including http or https: ")
+
+        if (!grepl("^https?://", ling_instance)) {
+
+          message("You have not provided a valid URL. Check it and be sure that contains http:// or https://.")
+          ling_instance <- readline("Please, type it again: ")
+
+          if (!grepl("^https?://", ling_instance)) {
+            stop("You have not provided a valid URL. Check it and be sure that contains http:// or https://.")
+          }
+        }
+
+        Sys.setenv(ling_inst = ling_instance)
+      }
+    }
+  }
+
+  str <- utils::URLencode(str, reserved = T)
+  req <- httr2::request(Sys.getenv("ling_inst")) |>
+    httr2::req_url_path_append("/api/v1") |>
+    httr2::req_url_path_append(from, to, str)
+
+  resp <- req |>
+    httr2::req_perform()
+
+  resp_details <- resp |>
+    httr2::resp_body_json()
+
+  translation <- resp_details$translation
+  return(translation)
+}
+
+
+
